26e4366cedd8c45044341f9efda6661d0200b32c
Marc Logan <marclogan90@gmail.com>
2022-10-31 13:06:30 +0100

    limited options on form page

--- src/pages/AddRestaurant.js
+++ src/pages/AddRestaurant.js
@@ -1,79 +1,90 @@
 import { useState } from "react";
 import axios from "axios";
-import cityAPI from '../utils/apiConnect'
+import cityAPI from "../utils/apiConnect";
 import { useNavigate } from "react-router-dom";
 import ListOfCities from "./ListOfCities";
-import Cuisines from "../cuisines.json"
+import Cuisines from "../cuisines.json";
 
 const API_URL = "http://localhost:5005";
 
 export default function AddRestaurant(props) {
+  const [cuisinesArray] = useState(Cuisines);
 
-    const[cuisinesArray] = useState(Cuisines)
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [rating, setRating] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [price, setPrice] = useState("");
+  const [cityId, setCityId] = useState("");
+  const navigate = useNavigate();
 
-    const [name, setName] = useState('')
-    const [address, setAddress] = useState('')
-    const [rating, setRating] = useState('')
-    const [cuisine, setCuisine] = useState('')
-    const [price, setPrice] = useState('')
-    const [cityId, setCityId] = useState('')
-    const navigate = useNavigate();
+  console.log(props);
+  console.log(cuisinesArray);
 
-    console.log(props)
-    console.log(cuisinesArray)
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const requestBody = { name, address, rating, cuisine, price, cityId };
+    cityAPI
+      .addRestaurant(requestBody)
+      .then((response) => {
+        setName("");
+        setAddress("");
+        setRating("");
+        setPrice("");
+        setCuisine("");
+        navigate("/cities");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-        const requestBody = { name, address, rating, cuisine, price, cityId };
-        cityAPI.addRestaurant(requestBody)
-            .then((response) => {
-                setName("")
-                setAddress("")
-                setRating("")
-                setPrice("")
-                setCuisine("")
-                navigate("/cities")
-            })
-            .catch((err) => { console.log(err) })
-    }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>City</label>
+        <select
+        placeholder="choose"
+          name="city"
+          onChange={(e) => setCityId(e.target.value)}
+        >
+            <option>Choose City</option>
+          {props.listOfCities.map((city) => {
+            return ( <>
+              <option key={city._id} value={city._id}>
+                {city.cityName}
+              </option> </>
+            );
+          })}
+        </select>
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <label>City</label>
-                <select defaultValue="Select" name="city" onChange={(e) => setCityId(e.target.value)}>
-                    {props.listOfCities.map((city) => {
-                        return (
-                            <option key={city._id} value={city._id}>{city.cityName}</option>
-                        )
-                    })}
-                </select>
+        <label>Address</label>
+        <input
+          type="text"
+          name="address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
 
-                <label>Address</label>
-                <input
-                    type="text"
-                    name="address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                />
+        <label>Rating</label>
+        <input
+          type="number"
+          name="rating"
+          min={1}
+          max={5}
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
 
-                <label>Rating</label>
-                <input
-                    type="number"
-                    name="rating"
-                    value={rating}
-                    onChange={(e) => setRating(e.target.value)}
-                />
-
-                {/* <label>Cuisine</label>
+        {/* <label>Cuisine</label>
                 <input
                     type="text"
                     name="cuisine"
@@ -81,26 +92,38 @@ export default function AddRestaurant(props) {
                     onChange={(e) => setCuisine(e.target.value)}
                 /> */}
 
-                <label>Cuisine2</label>
-                <select defaultValue="Select" name="cuisine" onChange={(e) => setCuisine(e.target.value)}>
-                    {cuisinesArray.map((each) => {
-                        console.log(each)
-                        return (
-                            <option>{each}</option>
-                        )
-                    })}
-                </select>
+        <label>Cuisine2</label>
+        <select
+          defaultValue="Select"
+          name="cuisine"
+          onChange={(e) => setCuisine(e.target.value)}
+        >
+          {cuisinesArray.map((each) => {
+            console.log(each);
+            return <option>{each}</option>;
+          })}
+        </select>
 
-                <label>Price</label>
-                <input
-                    type="text"
-                    name="price"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                />
+        {/* <label>Price</label>
+        <input
+          type="text"
+          name="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        /> */}
+        <label>Price</label>
+        <select
+          name="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        >
+          <option value="€">€</option>
+          <option value="€€">€€</option>
+          <option value="€€€">€€€</option>
+        </select>
 
-                <button> Submit </button>
-            </form>
-        </div>
-    )
+        <button> Submit </button>
+      </form>
+    </div>
+  );
 }
